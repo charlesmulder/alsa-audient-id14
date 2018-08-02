aaa55bcbb867321ae0c6a978a4f60a7f954f969e
Charles Mulder <1179063+charlesmulder@users.noreply.github.com>
2018-08-02 21:52:03 +0200

    printing out interfaces

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
 # WIP: Linux ALSA module for Audient iD14
 
+## To Do
+
+### Output
+
+* determine what end points are marked as isochronous out (feedback endpoint required?)
+* determine the size of the isochronous out end point that the device has
+* create buffers to hold the data that will be sent to the device
+* create a USB urb to write data to the device 
+
+### Input
+
+## Adhere to Kernel coding standards
+
+Please see ~/Products/cm/linux-stable/Documentation/development-process/4.Coding.rst
+
+## Quickstart
+
 ```bash
 # build the module using the kernel build system
 KERNELDIR=~/Products/cm/linux-stable/ make 
 # tail kernel output
 sudo dmseg -w
 # insert module into kernel
-insmod hello.ko 
+sudo insmod id14.ko 
+# remove module
+sudo rmmod id14
 ```
-
-## To Do
-
-### Adhere to Kernel coding standards
-
-Please see ~/Products/cm/linux-stable/Documentation/development-process/4.Coding.rst
-
 
 ## Resources
 
--- id14.c
+++ id14.c
@@ -5,12 +5,25 @@
 #include <linux/module.h>
 #include <linux/kref.h>
 #include <linux/usb.h>
-#include <linux/uaccess.h>
+//#include <linux/uaccess.h>
+#include <linux/usb/audio.h>
+#include <sound/core.h>
+#include <sound/initval.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
 //#include <alsa/asoundlib.h> 
 
 /* Define these values to match your devices */
 #define ID14_VENDOR_ID	0x2708 //0xfff0
 #define ID14_PRODUCT_ID	0x0002 //0xfff0
+
+/* module parameters (see "Module Parameters") */
+/* SNDRV_CARDS: maximum number of cards supported by this module */
+static int index[SNDRV_CARDS] = SNDRV_DEFAULT_IDX;
+static char *id[SNDRV_CARDS] = SNDRV_DEFAULT_STR;
+static bool enable[SNDRV_CARDS] = SNDRV_DEFAULT_ENABLE_PNP;
+
+static DEFINE_MUTEX(devices_mutex);
 
 /* table of devices that work with this driver */
 static struct usb_device_id id14_table [] = {
@@ -19,26 +32,68 @@ static struct usb_device_id id14_table [] = {
 };
 MODULE_DEVICE_TABLE (usb, id14_table);
 
+ /* Structure to hold all of our device specific stuff */
+struct usb_id14 {
+    struct usb_device * udev;           /* the usb device for this device */
+    struct usb_interface *  interface;      /* the interface for this device */
+    struct kref kref;
+};
+
 static int id14_probe(struct usb_interface *interface, const struct usb_device_id *id) 
 {
-    printk(KERN_INFO "Audient iD14 (%04X:%04X) plugged in\n", id->idVendor, id->idProduct);
 	struct usb_id14 *dev = NULL;
 	struct usb_host_interface *iface_desc;
-	struct usb_endpoint_descriptor *endpoint;
+    struct usb_host_endpoint *endpoint;
+	struct usb_endpoint_descriptor endpoint_desc;
+    struct usb_device *usb_dev = interface_to_usbdev(interface);
+    int interface_number;
+
+    //struct usb_host_config *config;
 	size_t buffer_size;
 	int i;
 	int retval = -ENOMEM;
+    int result;
 
+    //mutex_lock(&devices_mutex);
+    usb_get_dev(usb_dev);
+
+    printk(KERN_INFO "Audient iD14 (%04X:%04X) plugged in\n", id->idVendor, id->idProduct);
     pr_info("Audient iD14 probe function called");
     pr_info("Audient iD14 vendor id is: %u", id->idVendor); // 9992
     pr_info("Audient iD14 product id is: %u", id->idProduct); // 2
     pr_info("Audient iD14 device class: %u", id->bDeviceClass); // 0
     pr_info("Audient iD14 interface num_altsetting is: %u", interface->num_altsetting); // 1
+	//iface_desc = interface->cur_altsetting; // 0
 
-	iface_desc = interface->cur_altsetting;
+    pr_info("device number of configurations: %u", usb_dev->descriptor.bNumConfigurations); // 2
+    pr_info("device class: %u", usb_dev->descriptor.bDeviceClass); // 239 = misc
+    pr_info("device sub class: %u", usb_dev->descriptor.bDeviceSubClass); // 2
+    pr_info("device speed: %s", usb_speed_string(usb_dev->speed)); // high-speed
+    pr_info("device state: %s", usb_state_string(usb_dev->state)); // configured
+    pr_info("device number of interfaces: %u", usb_dev->actconfig->desc.bNumInterfaces); // 5
+    pr_info("device config value: %u", usb_dev->actconfig->desc.bConfigurationValue);
+
+    // printing out interfaces for active config
+    for(i = 0; i < usb_dev->actconfig->desc.bNumInterfaces; ++i) {
+        pr_info("i is %u", i);
+        pr_info("interface number of alt settings: %u", usb_dev->actconfig->interface[i]->num_altsetting);
+        pr_info("interface current alt setting: %s", usb_dev->actconfig->interface[i]->cur_altsetting->string);
+        pr_info("interface number of endpoints: %u", usb_dev->actconfig->interface[i]->cur_altsetting->desc.bNumEndpoints);
+        pr_info("interface class: %u", usb_dev->actconfig->interface[i]->cur_altsetting->desc.bInterfaceClass);
+        pr_info("interface subclass: %u", usb_dev->actconfig->interface[i]->cur_altsetting->desc.bInterfaceSubClass);
+        pr_info("interface number: %u", usb_dev->actconfig->interface[i]->cur_altsetting->desc.bInterfaceNumber);
+        pr_info("interface alternate setting: %u", usb_dev->actconfig->interface[i]->cur_altsetting->desc.bAlternateSetting);
+    }
+    //result = usb_set_interface(usb_dev, interface->cur_altsetting->desc.bInterfaceNumber, 0);
+    //mutex_unlock(&devices_mutex);
+    //USB_ENDPOINT_XFER_{CONTROL, ISOC, BULK, INT}
     return 0;
 }
 
+/**
+ * clean any private data that has been allocated 
+ * shut down any pending urbs that are in the USB system.
+ */
 static void id14_disconnect(struct usb_interface *interface) 
 {
     printk(KERN_INFO "Audient iD14 unplugged\n");
